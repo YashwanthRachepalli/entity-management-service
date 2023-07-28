2a12d43a128b3c9a71419087adbdcfd966a76f36
Yashwanth Rachepalli <yashwanthrachepalli@Yashwanths-MacBook-Air.local>
2023-07-28 16:07:14 +0530

    first commit

---
+++ src/main/java/com/ami/EntityManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityManagementServiceApplication.class, args);
+	}
+
+}
